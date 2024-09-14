9043d8d0ef392938f9998b7a7bc2935dcdaa694b
Håkon F. Fjellanger <hfredrif@gmail.com>
2024-09-14 16:07:17 +0000

    Complete exercise 4

--- Exercise_4/Exercise_4_2.cpp
+++ Exercise_4/Exercise_4_2.cpp
@@ -4,29 +4,62 @@ class Window : public Gtk::Window
 {
 public:
   Gtk::Box box;
-  Gtk::Entry entry;
+  Gtk::Label first_name_label;
+  Gtk::Entry first_name;
+  Gtk::Label last_name_label;
+  Gtk::Entry last_name;
   Gtk::Button button;
-  Gtk::Label label;
+  Gtk::Label full_name;
 
   Window() : box(Gtk::Orientation::ORIENTATION_VERTICAL)
   {
-    button.set_label("Click here");
+    this->set_title("Øving 4 ;P");
 
-    box.pack_start(entry);  // Add the widget entry to box
-    box.pack_start(button); // Add the widget button to box
-    box.pack_start(label);  // Add the widget label to box
+    button.set_label("Combine names");
+    button.set_sensitive(false);
+
+    first_name_label.set_text("First name");
+    last_name_label.set_text("Last name");
+
+    box.pack_start(first_name_label);
+    box.pack_start(first_name);
+    box.pack_start(last_name_label);
+    box.pack_start(last_name);
+    box.pack_start(button);
+    box.pack_start(full_name);
 
     add(box);   // Add vbox to window
     show_all(); // Show all widgets
 
-    entry.signal_changed().connect([this]()
-                                   { label.set_text("Entry now contains: " + entry.get_text()); });
+    first_name.signal_changed().connect([this]()
+                                        { entry_update(); });
 
-    entry.signal_activate().connect([this]()
-                                    { label.set_text("Entry activated"); });
+    last_name.signal_changed().connect([this]()
+                                       { entry_update(); });
 
     button.signal_clicked().connect([this]()
-                                    { label.set_text("Button clicked"); });
+                                    { button_click(); });
+  }
+
+private:
+  bool entries_populated()
+  {
+    return first_name.get_text().length() > 0 && last_name.get_text().length() > 0;
+  }
+
+  void entry_update()
+  {
+    button.set_sensitive(entries_populated());
+  }
+
+  void button_click()
+  {
+    std::string full_name_str;
+    full_name_str.append(first_name.get_text());
+    full_name_str.append(" ");
+    full_name_str.append(last_name.get_text());
+
+    full_name.set_text("Combined name: " + full_name_str);
   }
 };
 
